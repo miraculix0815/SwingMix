dcaca9e303be865c09fa89b801cdb7b488328c44
Jan Schlößin <jan.schloessin@klero.de>
2015-04-22 22:25:17 +0200

    inserts copyright notice in each file

--- src/swingmix/directories/DirectoryChooseDialog.java
+++ src/swingmix/directories/DirectoryChooseDialog.java
@@ -1,7 +1,35 @@
-/*
- * DirectoryChooseDialog.java
+/**
+ *  Copyright 2009-2015 Jan Schlößin
+ * 
+ *  This file is part of SwingMix.
  *
- * Created on 25.09.2009, 09:40:05
+ *  SwingMix is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SwingMix is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SwingMix.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Diese Datei ist Teil von SwingMix.
+ *
+ *  SwingMix ist Freie Software: Sie können es unter den Bedingungen
+ *  der GNU Lesser General Public License, wie von der Free Software Foundation,
+ *  Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ *  veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ *  SwingMix wird in der Hoffnung, dass es nützlich sein wird, aber
+ *  OHNE JEDE GEWÄHRLEISTUNG, bereitgestellt; sogar ohne die implizite
+ *  Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ *  Siehe die GNU Lesser General Public License für weitere Details.
+ *
+ *  Sie sollten eine Kopie der GNU Lesser General Public License zusammen mit diesem
+ *  Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
  */
 
 package swingmix.directories;
@@ -11,7 +39,9 @@ import swingmix.directories.DirectoriesTreePanel.DirectoryChangedListener;
 import java.io.File;
 
 /**
+ * DirectoryChooseDialog.java
  *
+ * Created on 25.09.2009, 09:40:05
  * @author jan
  */
 public class DirectoryChooseDialog extends JChangeDialog<DirectoryChooseDialog.FileContainer>
