3e730977f7fa4d8b6e9838b4f7c628d9e948863a
Jan Schlößin <jan@schloessin.de>
2020-10-06 16:02:27 +0200

    Don't perform EnterAction when a Table is in edit mode

--- src/swingmix/ui/JEscapeDialog.java
+++ src/swingmix/ui/JEscapeDialog.java
@@ -36,6 +36,7 @@ package swingmix.ui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.*;
 
 import javax.swing.*;
 
@@ -44,131 +45,124 @@ import javax.swing.*;
  *
  * @author Jan Schlößin
  */
-public class JEscapeDialog extends JDialog implements ContainerListener, KeyListener
-{
+public class JEscapeDialog extends JDialog implements ContainerListener, KeyListener {
 
-  public JEscapeDialog(Window owner, String title)
-  {
-       super(owner, title, ModalityType.APPLICATION_MODAL);
-       setLocationByPlatform(true);
-       addKeyAndContainerListenerRecursively(this);
+  public JEscapeDialog(Window owner, String title) {
+    super(owner, title, ModalityType.APPLICATION_MODAL);
+    setLocationByPlatform(true);
+    addKeyAndContainerListenerRecursively(this);
   }
 
-  public JEscapeDialog(Window parent)
-  {
-       this(parent, null);
+  public JEscapeDialog(Window parent) {
+    this(parent, null);
   }
 
   //The following function is recursive and is intended for internal use only. It is private to prevent anyone calling it from other classes
-//The function takes a Component as an argument and adds this Dialog as a KeyListener to it.
-//Besides it checks if the component is actually a Container and if it is, there  are 2 additional things to be done to this Container :
-//1 - add this Dialog as a ContainerListener to the Container
-//2 - call this function recursively for every child of the Container.
+  //The function takes a Component as an argument and adds this Dialog as a KeyListener to it.
+  //Besides it checks if the component is actually a Container and if it is, there  are 2 additional things to be done to this Container :
+  //1 - add this Dialog as a ContainerListener to the Container
+  //2 - call this function recursively for every child of the Container.
+  private void addKeyAndContainerListenerRecursively(Component c) {
+    //To be on the safe side, try to remove KeyListener first just in case it has been added before.
+    //If not, it won't do any harm
+    c.removeKeyListener(this);
+    //Add KeyListener to the Component passed as an argument
+    if (!(c instanceof JTextArea))
+      c.addKeyListener(this);
 
-  private void addKeyAndContainerListenerRecursively(Component c)
-  {
-      //To be on the safe side, try to remove KeyListener first just in case it has been added before.
+    if (c instanceof Container) {
+      Container cont = (Container) c;
+
+      //To be on the safe side, try to remove ContainerListener first just in case it has been added before.
       //If not, it won't do any harm
-      c.removeKeyListener(this);
-      //Add KeyListener to the Component passed as an argument
-      if (! (c instanceof JTextArea))
-        c.addKeyListener(this);
+      cont.removeContainerListener(this);
+      //Add ContainerListener to the Container.
+      cont.addContainerListener(this);
 
-       if(c instanceof Container){
+      //Get the Container's array of children Components.
+      Component[] children = cont.getComponents();
 
-//Component c is a Container. The following cast is safe.
-            Container cont = (Container)c;
-
-//To be on the safe side, try to remove ContainerListener first just in case it has been added before.
-//If not, it won't do any harm
-            cont.removeContainerListener(this);
-//Add ContainerListener to the Container.
-            cont.addContainerListener(this);
-
-//Get the Container's array of children Components.
-            Component[] children = cont.getComponents();
-
-//For every child repeat the above operation.
-            for(int i = 0; i < children.length; i++){
-                 addKeyAndContainerListenerRecursively(children[i]);
-            }
-       }
+      //For every child repeat the above operation.
+      for (Component child : children)
+        addKeyAndContainerListenerRecursively(child);
+    }
   }
 
+  //The following function is the same as the function above with the exception that it does exactly the opposite - removes this Dialog
+  //from the listener lists of Components.
+  private void removeKeyAndContainerListenerRecursively(Component c) {
+    c.removeKeyListener(this);
 
-//The following function is the same as the function above with the exception that it does exactly the opposite - removes this Dialog
-//from the listener lists of Components.
-
- private void removeKeyAndContainerListenerRecursively(Component c)
- {
-       c.removeKeyListener(this);
-
-       if(c instanceof Container){
-
-            Container cont = (Container)c;
-
-            cont.removeContainerListener(this);
-
-            Component[] children = cont.getComponents();
-
-            for(int i = 0; i < children.length; i++){
-                 removeKeyAndContainerListenerRecursively(children[i]);
-            }
-       }
+    if (c instanceof Container) {
+      Container cont = (Container) c;
+      cont.removeContainerListener(this);
+      Component[] children = cont.getComponents();
+      for (Component child : children)
+        removeKeyAndContainerListenerRecursively(child);
+    }
   }
 
-
-//ContainerListener interface
-/**********************************************************/
-
-//This function is called whenever a Component or a Container is added to another Container belonging to this Dialog
-  public void componentAdded(ContainerEvent e)
-  {
-       addKeyAndContainerListenerRecursively(e.getChild());
+  //This function is called whenever a Component or a Container is added to another Container belonging to this Dialog
+  @Override
+  public void componentAdded(ContainerEvent e) {
+    addKeyAndContainerListenerRecursively(e.getChild());
   }
 
-//This function is called whenever a Component or a Container is removed from another Container belonging to this Dialog
-  public void componentRemoved(ContainerEvent e)
-  {
-       removeKeyAndContainerListenerRecursively(e.getChild());
+  //This function is called whenever a Component or a Container is removed from another Container belonging to this Dialog
+  @Override
+  public void componentRemoved(ContainerEvent e) {
+    removeKeyAndContainerListenerRecursively(e.getChild());
   }
 
-/**********************************************************/
+  //This function is called whenever a Component belonging to this Dialog (or the Dialog itself) gets the KEY_PRESSED event
+  @Override
+  public void keyPressed(KeyEvent e) {
+    int code = e.getKeyCode();
+    if (code == KeyEvent.VK_ESCAPE)
+      performEscapeKeyAction();
 
-
-//KeyListener interface
-/**********************************************************/
-//This function is called whenever a Component belonging to this Dialog (or the Dialog itself) gets the KEY_PRESSED event
-  public void keyPressed(KeyEvent e)
-  {
-       int code = e.getKeyCode();
-       if(code == KeyEvent.VK_ESCAPE){
-//Key pressed is the ESCAPE key. Hide this Dialog.
-            setVisible(false);
-       }
-       else if(code == KeyEvent.VK_ENTER){
-//Key pressed is the ENTER key. Redefine performEnterAction() in subclasses to respond to depressing the ENTER key.
-            performEnterAction(e);
-       }
-
-//Insert code to process other keys here
+    else if (code == KeyEvent.VK_ENTER) {
+      if (! isATableIsInEditMode())
+        performEnterAction(e);
+    }
   }
 
-//We need the following 2 functions to complete imlementation of KeyListener
-  public void keyReleased(KeyEvent e)
-  {
+  private boolean isATableIsInEditMode() {
+    Component focusOwner = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusOwner();
+    return getTablesInComponentHierarchy(focusOwner).stream()
+            .anyMatch(JTable::isEditing);
   }
 
-  public void keyTyped(KeyEvent e)
-  {
+  private java.util.List<JTable> getTablesInComponentHierarchy(Component focusOwner) {
+    var tables = new ArrayList<JTable>();
+
+    Component inObservation = focusOwner;
+    while (inObservation != null) {
+      if (inObservation instanceof JTable)
+        tables.add((JTable) inObservation);
+
+      inObservation = inObservation.getParent();
+    }
+
+    return tables;
   }
 
-/************************************************************/
+  @Override
+  public void keyReleased(KeyEvent e) {
+  }
 
-  public void performEnterAction(KeyEvent e)
-  {
-//Default response to ENTER key pressed goes here
-//Redefine this function in subclasses to respond to ENTER key differently
+  @Override
+  public void keyTyped(KeyEvent e) {
+  }
+
+  public void performEnterAction(KeyEvent e) {
+    //Default response to ENTER key pressed goes here
+    //Redefine this function in subclasses to respond to ENTER key differently
+  }
+
+  public void performEscapeKeyAction() {
+    //Key pressed is the ESCAPE key. Hide this Dialog.
+    setVisible(false);
   }
 
 }
